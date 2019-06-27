cf399e163a287cd66f253a25b6bbf0242e818076
Zagozdon, Tomasz <tomasz.zagozdon@atos.net>
2019-06-27 10:37:37 +0200

    merged develop

--- src/api.ts
+++ src/api.ts
@@ -1,22 +1,20 @@
 import axios from 'axios';
+import { Todo } from "../types/types";
+
 const API_URL = 'http://localhost:3000';
 
-export function getTodos(): Promise<any> {
+export function getTodos(): Promise<Todo[]> {
     return axios.get(`${API_URL}/todos`);
 }
 
-export function postTodo(todo: any): Promise<any> {
-    return axios.get(`${API_URL}/todos`, todo);
+export function postTodo(todo: Todo): Promise<Todo> {
+    return axios.post(`${API_URL}/todos`, todo);
 }
 
-export function putTodo(todo: any): Promise<any>{
-    return axios.get(`${API_URL}/todos/${todo.id}`, todo);
+export function putTodo(todo: Todo): Promise<Todo>{
+    return axios.put(`${API_URL}/todos/${todo.id}`, todo);
 }
 
-export function deleteTodo(todo: any): Promise<any> {
-    return axios.get(`${API_URL}/todos/${todo.id}`);
-}
-
-export function deleteTodos(): Promise<any> {
-    return axios.get(`${API_URL}/todos`);
+export function deleteTodo(todo: Todo): Promise<Todo> {
+    return axios.delete(`${API_URL}/todos/${todo.id}`);
 }
