4040c793ec4499b1d2a7d6441b9f701b29308a5f
Strzempek <martyna.strzempek@atos.net>
2019-06-27 11:20:50 +0200

    import utils functions in app.ts

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,28 +1,28 @@
-export default {
-  uuid: () => {
-    /*jshint bitwise:false */
-    let i, random;
-    let uuid = '';
+export const uuid = () => {
+  /*jshint bitwise:false */
+  let i, random;
+  let uuid = '';
 
-    for (i = 0; i < 32; i++) {
-      random = Math.random() * 16 | 0;
-      if (i === 8 || i === 12 || i === 16 || i === 20) {
-        uuid += '-';
-      }
-      uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
+  for (i = 0; i < 32; i++) {
+    random = Math.random() * 16 | 0;
+    if (i === 8 || i === 12 || i === 16 || i === 20) {
+      uuid += '-';
     }
-
-    return uuid;
-  },
-  pluralize: (count, word) => {
-    return count === 1 ? word : word + 's';
-  },
-  store: function (namespace, data?) {
-    if (arguments.length > 1) {
-      return localStorage.setItem(namespace, JSON.stringify(data));
-    } else {
-      const store = localStorage.getItem(namespace);
-      return (store && JSON.parse(store)) || [];
-    }
+    uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
   }
-}
+
+  return uuid;
+};
+
+export const pluralize = (count, word) => {
+  return count === 1 ? word : word + 's';
+};
+
+export const store = function (namespace, data?) {
+  if (arguments.length > 1) {
+    return localStorage.setItem(namespace, JSON.stringify(data));
+  } else {
+    const store = localStorage.getItem(namespace);
+    return (store && JSON.parse(store)) || [];
+  }
+};
