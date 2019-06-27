79f1545f606b3cffa80d537c3b4b9dbe82df5a79
Strzempek <martyna.strzempek@atos.net>
2019-06-27 15:30:23 +0200

    remove commented lines

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,14 @@
 /*global jQuery, Handlebars, Router */
 import {Controller} from './controller';
 
-
-// enum StorageTypes {
-// 	REST_STORAGE = 'REST_STORAGE',
-// 	LOCAL_STORAGE = 'LOCAL_STORAGE'
-// }
-
 jQuery(function ($) {
 	'use strict';
+
+	const todoController = new Controller();
 
 	Handlebars.registerHelper('eq', function (a, b, options) {
 		return a === b ? options.fn(this) : options.inverse(this);
 	});
-
-	// const allStorages: { [key in StorageTypes]: Storage} = {
-	// 	[StorageTypes.REST_STORAGE]: new RestStorage(),
-	// 	[StorageTypes.LOCAL_STORAGE]: new LocalStorage()
-	// };
-
-	const todoController = new Controller();
 
 	todoController.init();
 });
